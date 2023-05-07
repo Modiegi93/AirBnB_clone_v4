00024bbb339fa7c0efcb0b90b37eb63e6b6df2f3
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-05-07 02:43:02 -0700

    final

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -39,5 +39,8 @@ $.ajax({
 	dataType: 'json',
 	data: '{}',
 	success: function (data) {
-		for (const place of Object.values(data))
-			$('section.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests
+		for (const place of Object.values(data)) {
+			$('section.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div>' + '<div class="number_rooms">' + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div>' + '<div class="number_bathrooms">' + '<i class="fa fa-bath fa-3x" aria-hidden+"true"></i><br />' + place.number_bathrooms + 'Bathrooms</div></div>' + '<div class="description">' + place.description + '</div></article>');
+	}
+}
+});
